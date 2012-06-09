51e0136247132d2bbfb7535ef6f45cdb6de18a93
Timothy J Fontaine <tjfontaine@gmail.com>
2012-06-09 14:31:57 -0400

    add track_info

--- examples/vlc.js
+++ examples/vlc.js
@@ -3,13 +3,17 @@ var vlc = require('../vlc');
 var media = vlc.mediaFromFile(process.argv[2]);
 media.parseSync();
 
+media.track_info.forEach(function (info) {
+  console.log(info);
+});
+
 console.log(media.artist, '--', media.album, '--', media.title);
 
 var player = vlc.mediaplayer;
 player.media = media;
 console.log('Media duration:', media.duration);
 
-player.play();
+//player.play();
 
 var poller = setInterval(function () {
   console.log('Poll:', player.position);
